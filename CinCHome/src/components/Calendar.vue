6dccb8026a55d16ef9343dc47387b87628df3d19
Jonah Moore <jonah.moore3@furman.edu>
2017-12-11 01:31:46 -0500

    Adding updated calendar component

--- CinCHome/src/components/Calendar.vue
+++ CinCHome/src/components/Calendar.vue
@@ -3,9 +3,18 @@
     <h1 class="t-center">CinC Events</h1>
     <vue-event-calendar
       :events="cincEvents"
-      @day-changed="handleDayChanged"
-      @month-changed="handleMonthChanged"
-    ></vue-event-calendar>
+    >
+
+      <template scope = "props">
+        <div v-for="events in props.showEvents" class="event-item" :key="events.title">
+          Date: {{events.date}}<br>
+          Title: {{events.title}}<br>
+          Description: {{events.desc}}<br>
+          Location: {{events.location}}<br>
+          Start Time: {{events.startTime}}
+        </div>
+      </template>
+    </vue-event-calendar>
 
   <v-card>
     <v-card-title>
@@ -41,13 +50,25 @@
           >
         </v-text-field>
         <v-text-field
+          label="Location"
+          v-model="location"
+          required
+          >
+        </v-text-field>
+        <v-text-field
+          label="Start Time"
+          v-model="startTime"
+          required
+          >
+        </v-text-field>
+        <v-text-field
           label="Email"
           v-model="email"
           required
         >
         </v-text-field>
 
-        <v-btn @click="submit">Submit</v-btn>
+        <v-btn @click="addEvent">Submit</v-btn>
       </v-form>
     </v-card-text>
   </div>
@@ -58,17 +79,24 @@
 <script>
 import axios from 'axios'
 
+// let today = new Date()
 export default {
   name: 'cal',
   data () {
     return {
       cincEvents: [{
-        // format = year/month/day
         date: `2017/12/25`,
         title: 'Christmas Day',
         desc: 'Time to see if Santa delivered me the winning powerball ticket.',
-        location: 'Johns Hall'
- 
+        location: 'Johns Hall',
+        startTime: '7:00'
+      },
+      {
+        date: `2017/12/24`,
+        title: 'test',
+        desc: 'testing',
+        location: 'testing again',
+        startTime: '4pm'
       }
       ],
       welcomeMessage: 'Suggest an Event',
@@ -90,17 +118,68 @@ export default {
       day: null,
       title: '',
       description: '',
+      location: '',
+      startTime: '',
       email: ''
     }
   },
   methods: {
-    handleDayChanged (data) {
-      console.log('date-changed', data)
+    getEvents () {
+      let self = this
+      axios.get('https://safe-beach-15501.herokuapp.com/api/calendar/events')
+      .then(response => {
+        console.log(response)
+        let temp = response.data
+        temp.forEach(obj => {
+          obj.date = self.getEventMonth(obj.date)
+        })
+        self.cincEvents = temp
+      }
+      )
     },
-    handleMonthChanged (data) {
-      console.log('month-changed', data)
+      // formats date into necessary format for the vue-event-calendar components
+      // to recognize it as a date in the calendar
+    getEventMonth (date) {
+      var firstLetYear = date.charAt(0)
+      var secondLetYear = date.charAt(1)
+      var thirdLetYear = date.charAt(2)
+      var fourthLetYear = date.charAt(3)
+      var firstLetMonth = date.charAt(5)
+      var secondLetMonth = date.charAt(6)
+      var firstLetDay = date.charAt(8)
+      var secondLetDay = date.charAt(9)
+      var properDate = firstLetYear + secondLetYear + thirdLetYear + fourthLetYear + '/' + firstLetMonth + secondLetMonth + '/' + firstLetDay + secondLetDay
+      return properDate
     }
   }
+    /* getEvents () {
+      axios.get('/api/calendarevents')
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+      },
+    addEvent () {
+      axios.post('/api/calendar/events/new') {
+        title: this.title,
+        email: this.email,
+        description: this.description,
+        location: this.location,
+        month: this.month,
+        day: this.day,
+        startTime: this.startTime
+      }
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+      }
+      */
+      // date, title, description
 }
 </script>
 
